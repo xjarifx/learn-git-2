6e7a2c2a22da875206f4db206bcc31b6604bafa2
jarif <xjarifx@gmail.com>
2026-03-05 19:37:57 +0600

    refactor: clean up main.js and remove unused files

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
-console.log("Hello from main.js");
-console.log("Hello from main.js again");
-console.log("Hello from main.js again and again");
-console.log("Hello from main.js again and again and again");
-console.log("Hello from main.js again and again and again and again");
+console.log("main.js");
--- v11.js
+++
@@ -1,1 +0,0 @@
-console.log("hello from v11.js");
